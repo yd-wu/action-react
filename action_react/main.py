76f72568c03398f9c107fecc9803af99619dbc3b
yd-wu <yw3960@columbia.edu>
2023-04-05 09:13:39 -0400

    updated docstrings

--- action_react/main.py
+++ action_react/main.py
@@ -9,6 +9,22 @@ FILENAME = "config.ini"
 
 
 def read_configs(file_name):
+    """
+    Read configuration parameters from a file.
+
+    Parameters
+    ----------
+    file_name: str, relative path from the root directory of this package
+
+    Returns
+    -------
+    api_id: telegram api id
+    api_hash: telegram api hash
+    phone: telegram phone number
+    username: telegram username
+    target_date: the date upon which to improve
+    cities: list of city names
+    """
     config = configparser.ConfigParser()
     config.read(file_name)
     api_id = config["Telegram"]["api_id"]
@@ -21,6 +37,13 @@ def read_configs(file_name):
 
 
 def get_target_channel():
+    """
+    Read telegram channel to listen to from command line.
+
+    Returns
+    -------
+    listening_channel: channel name
+    """
     listening_channel = input("enter entity (telegram URL or entity id) to listen to: ")
     if listening_channel.isdigit():
         return PeerChannel(int(listening_channel))
@@ -28,12 +51,42 @@ def get_target_channel():
 
 
 def connect_to_telegram(username, api_id, api_hash, phone):
+    """
+    Connect to telegram
+
+    Parameters
+    ----------
+    username: str, user name
+    api_id: str, telegram api id
+    api_hash: str, telegram api hash
+    phone: str, phone number
+
+    Returns
+    -------
+    client: telegram client
+    """
     client = TelegramClient(username, api_id, api_hash)
     client.start()
     return client
 
 
 def check_message(message, target_date, cities):
+    """
+    Process incoming telegram messages
+
+    Parameters
+    ----------
+    message: str, message
+    target_date: date, the date upon which to improve
+    cities: list of city names
+
+    Returns
+    -------
+    A tuple. The first element indicates whether a good date
+    can be retrieved from the message. The second element is None
+    if the first element is False. Otherwise, it's a tuple whose
+    first element is the date and the second element is the city
+    """
     try:
         colon_list = message.split(":")
         message_list = [" ".join(colon_list[0].split()[:-1])] + [colon_list[0].split()[-1]] + colon_list[1].split()
@@ -52,6 +105,19 @@ def check_message(message, target_date, cities):
 
 
 def main(api_id, api_hash, phone, username, target_date, cities):
+    """
+    Main function that acts on a target telegram message. It runs
+    until interrupted.
+
+    Parameters
+    ----------
+    api_id: str, telegram api id
+    api_hash: str, telegram api hash
+    phone: str, telegram phone number
+    username: str, telegram username
+    target_date: date, the date upon which to improve
+    cities: [str], list of city names
+    """
     listening_channel = get_target_channel()
     client = connect_to_telegram(username, api_id, api_hash, phone)
 
@@ -62,6 +128,7 @@ def main(api_id, api_hash, phone, username, target_date, cities):
         is_good, info = check_message(message, target_date, cities)
         if is_good:
             date, city = info
+            # TODO: expand the action part
             print(f"found a good appointment in {city} on {date} ")
 
     with client:
--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,9 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+from recommonmark.transform import AutoStructify
 
 # -- Project information -----------------------------------------------------
-
 project = 'action-react'
 copyright = '2023, yd-wu'
 author = 'yd-wu'
@@ -24,14 +24,15 @@ author = 'yd-wu'
 # The full version, including alpha/beta/rc tags
 release = '0.0.2'
 
+master_doc="index"
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['recommonmark','sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+source_suffix = ['.rst', '.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -53,3 +54,9 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        'auto_toc_tree_section': 'Contents',
+    }, True)
+    app.add_transform(AutoStructify)
